e84d3cb8e8c992cedf21a7d39fa6c844931067ca
Bruno Brito <bruno@git-tower.com>
2025-03-24 15:42:41 +0000

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+  site: 'https://git-flow-next.dev',
+  integrations: [sitemap()],
 });
